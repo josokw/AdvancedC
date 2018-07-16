71ea66cb3d067d5a77c14aac652d76839051c588
jos <jos.onokiewicz@han.nl>
2018-07-16 19:21:30 +0200

    Added voidPointers code.

---
+++ voidPointers/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int iVar = 1;
+   double dVar = 2.2;
+   char cVar = 'c';
+   void *pGeneric = NULL;
+   
+   pGeneric = &iVar;
+   printf(" pGeneric = %p points to %d\n", pGeneric, *(int*)pGeneric);
+
+   pGeneric = &dVar;
+   printf(" pGeneric = %p points to %lf\n", pGeneric, *(double*)pGeneric);
+
+   pGeneric = &cVar;
+   printf(" pGeneric = %p points to %c\n", pGeneric, *(char*)pGeneric);
+
+   return 0;
+}
