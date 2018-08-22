544ca85aacf2e0790accde247e8090ca9794ea4c
jos <jos.onokiewicz@han.nl>
2018-08-22 15:30:15 +0200

    Improved code layout, added more comments

--- function-pointers/main.c
+++ function-pointers/main.c
@@ -6,18 +6,21 @@ int multiply(int a, int b);
 int main(void)
 {
    int result = 0;
-   /* function pointer declaration */
+   /* function pointer variable declaration */
    int (*fpointer)(int, int) = add;
 
    printf("Address 'add()'      = %p\nAddress 'fpointer'   = %p\n", add,
           fpointer);
+
    /* function call by function pointer */
    result = fpointer(2, 5);
    printf("result = %d\n\n", result);
 
+   /* update function pointer variable */
    fpointer = multiply;
    printf("Address 'multiply()' = %p\nAddress 'fpointer'   = %p\n",
           multiply, fpointer);
+
    /* function call by function pointer */
    result = fpointer(2, 5);
    printf("result = %d\n", result);
