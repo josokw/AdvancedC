947e83785725d5084fc6f6e1a1e0eaeee4ad4b86
jos <jos.onokiewicz@han.nl>
2019-08-24 15:53:09 +0200

    Added a comment, more output

--- pointerArray/main.c
+++ pointerArray/main.c
@@ -4,21 +4,20 @@
 
 int main(void)
 {
-   char *stringData[SIZE] = {"Hello", "world", "of", "pointers"};
+   /* Every string should be considered a pointer to the string */
+   const char *stringData[SIZE] = {"Hello", "world", "of", "pointers"};
    int i = 0;
 
    for (i = 0; i < SIZE; i++)
    {
-      printf("%s ", stringData[i]);
+      printf("stringData[%d] = \"%s\"\n", i, stringData[i]);
    }
    puts("\n");
 
-   /* Only printing the first character of every string
-    * separated by a space.
-    */
+   /* Only printing the first character of every string */
    for (i = 0; i < SIZE; i++)
    {
-      printf("%c ", stringData[i][0]);
+      printf("stringData[%d][0] = '%c'\n", i, stringData[i][0]);
    }
    puts("\n");
 
