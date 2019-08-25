5ec96a29841a66c64205d01db76d3c6c0261f04d
jos <jos.onokiewicz@han.nl>
2019-08-25 15:30:43 +0200

    Added hard coded check for size

--- arraySize/main.c
+++ arraySize/main.c
@@ -7,14 +7,21 @@ void printArraySize(const double b[]);
 int main(void)
 {
    double buffer[ARRAYSIZE] = {1.0, 2.0, 3.0, 4.0};
+   size_t size = 0;
 
    printf("Size double    = %zu bytes\n\n", sizeof(double));
    /* buffer is an array type variable */
    printf("Size buffer    = %zu bytes\n", sizeof(buffer));
    printf("Size buffer[0] = %zu bytes\n", sizeof(buffer[0]));
-   printf("Number of data values in buffer = %zu\n\n",
-          sizeof(buffer) / sizeof(buffer[0]));
+   size = sizeof(buffer) / sizeof(buffer[0]);
+   printf("Number of data values in buffer = %zu  ", size);
 
+   if (size == ARRAYSIZE)
+   {
+      printf("is correct\n");
+   }
+   puts("");
+   
    /* buffer becomes pointer type! */
    printArraySize(buffer);
 
@@ -23,9 +30,14 @@ int main(void)
 
 void printArraySize(const double b[])
 {
+   size_t size = 0;
    /* b is an array pointer type: double* */
    printf("Size b         = %zu bytes\n", sizeof(b));
    printf("Size b[0]      = %zu bytes\n", sizeof(b[0]));
-   printf("Number of data values in buffer = %zu IS WRONG\n\n",
-          sizeof(b) / sizeof(b[0]));
+   size = sizeof(b) / sizeof(b[0]);
+   printf("Number of data values in array b = %zu  ", size);
+   if (size != ARRAYSIZE)
+   {
+      printf("is not correct\n");
+   }
 }
