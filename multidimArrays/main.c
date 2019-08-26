2d4d844ce4ac684a79f5c5bf2c530d526cfdecb2
jos <jos.onokiewicz@han.nl>
2019-08-26 10:40:28 +0200

    Replaced int by size_t

--- multidimArrays/main.c
+++ multidimArrays/main.c
@@ -4,9 +4,9 @@
 #define MAX_COLUMN 3
 #define MAX_DEPTH 2
 
-void print1DimArray(int ar1D[], int size);
-void print2DimArray(int ar2D[][MAX_COLUMN], int size);
-void print3DimArray(int ar3D[][MAX_COLUMN][MAX_DEPTH], int size);
+void print1DimArray(int ar1D[], size_t size);
+void print2DimArray(int ar2D[][MAX_COLUMN], size_t size);
+void print3DimArray(int ar3D[][MAX_COLUMN][MAX_DEPTH], size_t size);
 
 int main(void)
 {
@@ -17,56 +17,53 @@ int main(void)
 
    int data3D[MAX_ROW][MAX_COLUMN][MAX_DEPTH] = {
       {{100, 110}, {200, 210}, {300, 310}},
-
       {{101, 111}, {201, 211}, {301, 311}},
-
       {{102, 122}, {202, 222}, {302, 322}},
-
       {{103, 133}, {203, 233}, {303, 333}}};
 
    printf("---- 1 dim array (%d):\n", MAX_ROW);
    print1DimArray(data1D, MAX_ROW);
 
-   printf("---- 2 dim array (%d x %d):\n", MAX_ROW, MAX_COLUMN);
+   printf("\n---- 2 dim array (%d x %d):\n", MAX_ROW, MAX_COLUMN);
    print2DimArray(data2D, MAX_ROW);
 
-   printf("---- 3 dim array (%d x %d x %d):\n", MAX_ROW, MAX_COLUMN,
+   printf("\n---- 3 dim array (%d x %d x %d):\n", MAX_ROW, MAX_COLUMN,
           MAX_DEPTH);
    print3DimArray(data3D, MAX_ROW);
 
    return 0;
 }
 
-void print1DimArray(int ar1D[], int size)
+void print1DimArray(int ar1D[], size_t size)
 {
-   int i = 0;
+   size_t i = 0;
    for (i = 0; i < size; i++)
    {
-      printf("%d ", ar1D[i]);
+      printf("%4d ", ar1D[i]);
    }
    puts("");
 }
 
-void print2DimArray(int ar2D[][MAX_COLUMN], int size)
+void print2DimArray(int ar2D[][MAX_COLUMN], size_t size)
 {
-   int i = 0;
-   int j = 0;
+   size_t i = 0;
+   size_t j = 0;
 
    for (i = 0; i < size; i++)
    {
       for (j = 0; j < MAX_COLUMN; j++)
       {
-         printf("%d ", ar2D[i][j]);
+         printf("%4d ", ar2D[i][j]);
       }
       puts("");
    }
 }
 
-void print3DimArray(int ar3D[][MAX_COLUMN][MAX_DEPTH], int size)
+void print3DimArray(int ar3D[][MAX_COLUMN][MAX_DEPTH], size_t size)
 {
-   int i = 0;
-   int j = 0;
-   int k = 0;
+   size_t i = 0;
+   size_t j = 0;
+   size_t k = 0;
 
    for (k = 0; k < MAX_DEPTH; k++)
    {
@@ -74,7 +71,7 @@ void print3DimArray(int ar3D[][MAX_COLUMN][MAX_DEPTH], int size)
       {
          for (j = 0; j < MAX_COLUMN; j++)
          {
-            printf("%d ", ar3D[i][j][k]);
+            printf("%4d ", ar3D[i][j][k]);
          }
          puts("");
       }
