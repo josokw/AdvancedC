0609267f5058e6c07723d11a28c34e47930c7fcb
jos <jos.onokiewicz@han.nl>
2019-08-29 01:19:30 +0200

    Added compare

---
+++ compare/main.c
@@ -0,0 +1,65 @@
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+
+#define EPSILON 1e-9
+
+int fequal(double a, double b);
+
+int main(void)
+{
+   double d1 = 0.1;
+   double d2 = 0.2;
+   float f1 = 0.1;
+
+   printf(
+      "Smallest double value != 0.0\n"
+      "        = %.17lf\n\n",
+      DBL_EPSILON);
+   printf(
+      "Smallest float value != 0.0\n"
+      "        = %.17f\n\n",
+      FLT_EPSILON);
+
+   printf("d1      = %.17lf\n", d1);
+   printf("f1      = %.17f\n", f1);
+   printf("d2      = %.17lf\n", d2);
+   printf("d1 + d2 = %.17lf\n\n", d1 + d2);
+
+   if (d1 == f1)
+   {
+      printf("double 0.1 == float 0.1\n");
+   }
+   else
+   {
+      printf("double 0.1 != float 0.1\n");
+   }
+   printf("\n");
+
+   if (d1 + d2 == 0.3)
+   {
+      printf("d1 + d2 == 0.3   0.1 + 0.2 == 0.3\n");
+   }
+   else
+   {
+      printf("d1 + d2 != 0.3   0.1 + 0.2 != 0.3\n");
+   }
+   puts("");
+
+   if (fequal(d1 + d2, 0.3))
+   {
+      printf("fequal(d1 + d2, 0.3)   0.1 + 0.2 == 0.3\n");
+   }
+   else
+   {
+      printf("fequal(d1 + d2, 0.3)   0.1 + 0.2 != 0.3\n");
+   }
+   puts("");
+
+   return 0;
+}
+
+int fequal(double a, double b)
+{
+   return fabs(a - b) < EPSILON;
+}
