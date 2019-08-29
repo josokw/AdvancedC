c10a685e2399d935c9804c7443b529fe55960fbb
jos <jos.onokiewicz@han.nl>
2019-08-29 16:09:16 +0200

    Improved output texts

--- mixedtypes/main.c
+++ mixedtypes/main.c
@@ -6,24 +6,21 @@ int main(void)
    unsigned int uiB = 0;
    int iC = -567;
 
-   printf("Mixed types in expressions will cause problems\n\n");
+   printf("Mixed types in expressions will cause problems!\n\n");
+
    uiB = uiA + iC;
    printf(
-      "iC = %d is casted to unsigned int in mixed type expression: %u\n",
-      iC, uiB);
+      "uiB = uiA + iC;\n"
+      "iC = %d is casted to unsigned int: %u\n"
+      "Mixed type expression causes: uiB = %u\n\n",
+      iC, iC, uiB);
 
    if (uiA < iC)
    {
-      printf("Mixed type expression causes: 123 < -567\n");
-   }
-   else
-   {
-      printf("OK\n");
-   }
-
-   if ((int)uiA < iC)
-   {
-      printf("123 < -567\n");
+      printf(
+         "uiA < iC\n"
+         "iC = %d is casted to unsigned int: %u\n"
+         "Mixed type expression causes: 123 < -567 to be true\n\n", iC, iC);
    }
    else
    {
