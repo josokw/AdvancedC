c262768bd8eff1dd636d07d1a52f2473115f0448
jos <jos.onokiewicz@han.nl>
2019-08-29 15:17:30 +0200

    Added more info to output

--- atoiatof/main.c
+++ atoiatof/main.c
@@ -6,20 +6,22 @@ int main(void)
    printf(
       "-- int atoi(const char *str);\n"
       "-- Printing integers:\n");
-   printf("%11d \n", atoi("123"));
-   printf("%11d \n", atoi("123.4"));
-   printf("%11d \n", atoi("123abc"));
-   printf("%11d \n", atoi("abc123"));
-   printf("\n");
+
+   printf("   123    => %8d \n", atoi("123"));
+   printf("   123.4  => %8d \n", atoi("123.4"));
+   printf("   123abc => %8d \n", atoi("123abc"));
+   printf("   abc123 => %8d \n", atoi("abc123"));
+   puts("");
 
    printf(
       "-- double atof(const char *str);\n"
       "-- Printing doubles:\n");
-   printf("%11lf \n", atof("123.012"));
-   printf("%11lf \n", atof("123.012abc"));
-   printf("%11lf \n", atof("123"));
-   printf("%11lf \n", atof("abc123"));
-   printf("\n");
+      
+   printf("   123.012    => %11lf \n", atof("123.012"));
+   printf("   123.012abc => %11lf \n", atof("123.012abc"));
+   printf("   123        => %11lf \n", atof("123"));
+   printf("   abc123     => %11lf \n", atof("abc123"));
+   puts("");
 
    return 0;
 }
