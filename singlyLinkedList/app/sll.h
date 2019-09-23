5ab50507bb148588020c1a7b7e30ba7c0954013d
jos <jos.onokiewicz@han.nl>
2019-09-23 12:12:14 +0200

    Removed duplicated line

--- singlyLinkedList/app/sll.h
+++ singlyLinkedList/app/sll.h
@@ -9,20 +9,22 @@ typedef struct node {
    struct node *pNextNode;
 } node_t;
 
-void showSLL(const node_t *pHead);
 /*! Returns the size of the SLL (equals the number of nodes).
  * \todo Implement function.
  */
 size_t sizeSLL(const node_t *pHead);
+
 /*! Adds data in a new node at the end of the SLL. */
 void addSLL(node_t **ppHead, int data);
+
 /*! Empties the SLL.
  * \pre pHead == NULL
  */
 void clearSLL(node_t **ppHead);
+
 /*! Shows in ASCII the contents of all nodes from the head to the end 
  * of the list. 
- * */
+ */
 void showSLL(const node_t *pHead);
 
 #endif
